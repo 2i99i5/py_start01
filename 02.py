52c92773cc4685de5da0ffb5626d8c224f68aba8
2i99i5 <zakirovra@gmail.com>
2019-10-09 14:35:45 +0400

    правки под PEP-8

--- 02.py
+++ 02.py
@@ -4,7 +4,7 @@
 
 num = input("Введите число: ")
 if num.isdigit():  # проверка ввода
-    sum = int(num) + int(num * 2) + int(num * 3)
-    print(f"Cумма равна {sum}")
+    sum_num = int(num) + int(num * 2) + int(num * 3)
+    print(f"Cумма равна {sum_num}")
 else:
-	print("Необходимо ввести число, программа завершена")
+    print("Необходимо ввести число, программа завершена")
