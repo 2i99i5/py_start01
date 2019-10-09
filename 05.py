5c68e8fd899790071ca3f296198a5faa88e96cfa
2i99i5 <zakirovra@gmail.com>
2019-10-09 16:17:02 +0400

    Правка для подсчета общей дистанции за все дни

--- 05.py
+++ 05.py
@@ -4,14 +4,16 @@
 
 a = input("Результат пробежки в первый день: ")
 b = input("Требуемый общий результат: ")
-
+total = 0  # общая дистанция
 if a.isdigit() and b.isdigit():  # проверка ввода
     count = 1  # кол-во дней
     a = int(a)
     b = int(b)
-    while a < b:
+    total = a
+    while total < b:
         a = a * 1.1
+        total = total + a
         count += 1
-    print(f"На {count} день спортсмен пробежал более {b}")
+    print(f"На {count} день спортсмен достиг результата {b}")
 else:
     print("Необходимо ввести числа, программа завершена")
